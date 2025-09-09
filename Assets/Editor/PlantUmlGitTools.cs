0988c96d9009d1366c7ab82872e57ef53132fa70
EBIchannnn <hagurumarazaroy@gmail.com>
2025-09-10 01:05:12 +0900

    25

---
+++ Assets/Editor/PlantUmlGitTools.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+
+public static class PlantUmlGitTools
+{
+    const string InputDir  = "Assets/Script";   // 必要なら Scripts に変更
+    const string OutputDir = "Docs/uml";
+    const string PlantUmlJar = "plantuml.jar";  // Actions で curl で取得した jar を使う
+
+    public static void Main(string[] args)
+    {
+        Directory.CreateDirectory(OutputDir);
+
+        Run("puml-gen", $"{InputDir} {OutputDir} -dir -ignore bin,obj,Properties -createAssociation -allInOne");
+        Run("java", $"-jar \"{PlantUmlJar}\" -tsvg \"{OutputDir}\"");
+    }
+
+    static void Run(string file, string arguments)
+    {
+        var p = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = file,
+                Arguments = arguments,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = true
+            }
+        };
+        p.Start();
+        Console.WriteLine(p.StandardOutput.ReadToEnd());
+        var err = p.StandardError.ReadToEnd();
+        if (!string.IsNullOrEmpty(err))
+            Console.WriteLine("[WARN] " + err);
+        p.WaitForExit();
+    }
+}
