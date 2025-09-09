5c9a7dcbe6f24a5bcb07bab38f2e512211cf5737
EBIchannnn <hagurumarazaroy@gmail.com>
2025-09-10 02:16:22 +0900

    38

--- Assets/Editor/PlantUmlGitTools.cs
+++ Assets/Editor/PlantUmlGitTools.cs
@@ -4,7 +4,7 @@ using System.IO;
 
 public static class PlantUmlGitTools
 {
-    const string InputDir  = "Assembly-CSharp.csproj";   // 必要なら Scripts に変更
+    const string InputDir  = "Assets/Script";   // 必要なら Scripts に変更
     const string OutputDir = "Docs/uml";
     const string PlantUmlJar = "plantuml.jar";  // Actions で curl で取得した jar を使う
 
