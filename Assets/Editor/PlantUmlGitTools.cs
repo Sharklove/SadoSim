5a1757bc79de317a665fec1c7102ea1e2c7ba3f2
EBIchannnn <hagurumarazaroy@gmail.com>
2025-09-10 23:51:27 +0900

    43

--- Assets/Editor/PlantUmlGitTools.cs
+++ Assets/Editor/PlantUmlGitTools.cs
@@ -4,7 +4,7 @@ using System.IO;
 
 public static class PlantUmlGitTools
 {
-    const string InputDir  = "Assets/Script";   // 必要なら Scripts に変更
+    const string InputDir  = "Assets/Script/Sample";   // 必要なら Scripts に変更
     const string OutputDir = "Docs/uml";
     const string PlantUmlJar = "plantuml.jar";  // Actions で curl で取得した jar を使う
     static readonly string toolPath = Path.Combine(Environment.GetEnvironmentVariable("HOME"), ".dotnet", "tools", "puml-gen");
