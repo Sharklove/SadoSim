c3e3e8729d49418d67bd07bcb9126a8eb46df751
EBIchannnn <hagurumarazaroy@gmail.com>
2025-09-23 22:24:12 +0900

    方向転換追加

---
+++ Assets/Script/Direction.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Direction : MonoBehaviour
+{
+    public float rotationSpeed = 100f;
+
+    void Update()
+    {
+        // マウスのX軸の動きを取得
+        float mouseX = Input.GetAxis("Mouse X");
+
+        // オブジェクトのY軸を中心に回転
+        transform.Rotate(Vector3.up * mouseX * rotationSpeed * Time.deltaTime);
+    }
+}
