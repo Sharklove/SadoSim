88fd2f9b50a3166fcb4e7a625bfd718f359eb31e
EBIchannnn <hagurumarazaroy@gmail.com>
2025-09-25 23:37:26 +0900

    シーン編集

--- Assets/Script/Move.cs
+++ Assets/Script/Move.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class Move : MonoBehaviour
 {
-    public float moveSpeed = 5f;    // 移動速度
+    public float moveSpeed = 2f;    // 移動速度
     public float gravity = -9.81f;  // 重力加速度
 
     private CharacterController controller;
@@ -17,7 +17,7 @@ public class Move : MonoBehaviour
     void Update()
     {
         if (UIManager.IsUIActive) return;
-        if (Sit.isCrouching) return; // しゃがんでいるときは移動を無効化
+        if (Sit.isSiting) return; // しゃがんでいるときは移動を無効化
         // WASD 入力を取得（x=横移動, z=前後移動）
         float h = Input.GetAxisRaw("Horizontal"); // A,Dキー or ←,→
         float v = Input.GetAxisRaw("Vertical");   // W,Sキー or ↑,↓
