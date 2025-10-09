02ce01eef3bbcccfb65e313ffd5376324e11832a
EBIchannnn <hagurumarazaroy@gmail.com>
2025-10-10 00:07:26 +0900

    障子動くようにした

--- Assets/Scripts/DotweenMover.cs
+++ Assets/Scripts/DotweenMover.cs
@@ -3,16 +3,30 @@ using DG.Tweening;
 
 public class DotweenMover : MonoBehaviour
 {
-    public Transform targetTransform; // オブジェクトを指定する場合
-    public Vector3 targetPosition;    // 座標で指定する場合
-    public bool useTransform = false;  // どちらを使うか選択
+    // public Transform targetTransform; // オブジェクトを指定する場合
+    // public bool useTransform = false;  // どちらを使うか選択
+    public Vector3 originalPosition;  // 元の座標
+    public Vector3 targetPosition;    // 目的地の座標
     public float duration = 2f;
+    public bool moveParent = false; // 親オブジェクトを動かすかどうか
+    public bool moved = false;
 
     public void MoveToTarget()
     {
-        Vector3 destination = useTransform ? targetTransform.position : targetPosition;
-
-        transform.DOMove(destination, duration)
-                 .SetEase(Ease.InOutSine);
+        // Vector3 destination = useTransform ? targetTransform.position : targetPosition;
+        Transform target = moveParent ? transform.parent : transform;
+        if (moved)
+        {
+            
+            target.DOMove(originalPosition, duration)
+                  .SetEase(Ease.InOutSine);
+            
+            moved = false;
+        } else {
+            target.DOMove(targetPosition, duration)
+                  .SetEase(Ease.InOutSine);
+            
+            moved = true;
+        }
     }
 }
